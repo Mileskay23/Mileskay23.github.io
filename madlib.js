5efc42142ec5bad7298351ef718cf0b5d6b08c80
Mileskay23 <820293@mystma.com>
2022-04-26 11:31:22 -0500

    Newst update

---
+++ madlib.js
@@ -0,0 +1,116 @@
+function checkPass(){
+	if (document.getElementById("password").value == "underwater"){
+    document.getElementById("access").innerHTML = "CORRECT"
+	}	
+	else{
+		document.getElementById("access").innerHTML = "WRONG"
+	}	
+}
+
+document.getElementById("submit").addEventListener("click", checkPass)
+
+
+function checkPass2(){
+	if (document.getElementById("password2").value == "seaweed"){
+    document.getElementById("access2").innerHTML = "CORRECT"
+	}	
+	else{
+		document.getElementById("access2").innerHTML = "WRONG"
+	}	
+}
+
+document.getElementById("submit2").addEventListener("click", checkPass2)
+
+function checkPass3(){
+	if (document.getElementById("password3").value == "sizes"){
+    document.getElementById("access3").innerHTML = "CORRECT"
+	}	
+	else{
+		document.getElementById("access3").innerHTML = "WRONG"
+	}	
+}
+
+document.getElementById("submit3").addEventListener("click", checkPass3)
+
+function checkPass4(){
+	if (document.getElementById("password4").value == "small"){
+    document.getElementById("access4").innerHTML = "CORRECT"
+	}	
+	else{
+		document.getElementById("access4").innerHTML = "WRONG"
+	}	
+}
+
+document.getElementById("submit4").addEventListener("click", checkPass4)
+
+function checkPass5(){
+	if (document.getElementById("password5").value == "world"){
+    document.getElementById("access5").innerHTML = "CORRECT"
+	}	
+	else{
+		document.getElementById("access5").innerHTML = "WRONG"
+	}	
+}
+
+document.getElementById("submit5").addEventListener("click", checkPass5)
+
+function checkPass6(){
+	if (document.getElementById("password6").value == "food"){
+    document.getElementById("access6").innerHTML = "CORRECT"
+	}	
+	else{
+		document.getElementById("access6").innerHTML = "WRONG"
+	}	
+}
+
+document.getElementById("submit6").addEventListener("click", checkPass6)
+
+function checkPass7(){
+	if (document.getElementById("password7").value == "sushi"){
+    document.getElementById("access7").innerHTML = "CORRECT"
+	}	
+	else{
+		document.getElementById("access7").innerHTML = "WRONG"
+	}	
+}
+
+document.getElementById("submit7").addEventListener("click", checkPass7)
+
+function checkPass8(){
+	if (document.getElementById("password8").value == "country"){
+    document.getElementById("access8").innerHTML = "CORRECT"
+	}	
+	else{
+		document.getElementById("access8").innerHTML = "WRONG"
+	}	
+}
+
+document.getElementById("submit8").addEventListener("click", checkPass8)
+
+function checkPass9(){
+	if (document.getElementById("password9").value == "anime"){
+    document.getElementById("access9").innerHTML = "CORRECT"
+	}	
+	else{
+		document.getElementById("access9").innerHTML = "WRONG"
+	}	
+}
+
+document.getElementById("submit9").addEventListener("click", checkPass9)
+
+function checkPass10(){
+	if (document.getElementById("password10").value == "travel"){
+    document.getElementById("access10").innerHTML = "CORRECT"
+	}	
+	else{
+		document.getElementById("access10").innerHTML = "WRONG"
+	}	
+}
+
+document.getElementById("submit10").addEventListener("click", checkPass10)
+
+
+
+
+
+
